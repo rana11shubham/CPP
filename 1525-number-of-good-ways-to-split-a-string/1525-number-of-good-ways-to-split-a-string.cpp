8f772b438891cf6d05858c922b940b93fbdedf4e
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-01-19 01:05:35 +0530

    Time: 68 ms (16.61%), Space: 10.4 MB (33.42%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -2,37 +2,31 @@ class Solution {
 public:
     int numSplits(string s) {
         int n=s.length();
-        vector<int>left(n,0);
-        vector<int>right(n,0);
-        unordered_map<char,int>mp;
-        for(int i=1;i<n;i++){
-            if(mp.find(s[i-1])!=mp.end()){
-                mp[s[i-1]]++;
-                left[i]=left[i-1];
-            }
-            else{
-                left[i]=left[i-1]+1;
-                mp[s[i-1]]++;
-            }
-        }
-        mp.clear();
-        for(int i=n-2;i>=0;i--){
-            char ch=s[i+1];
-            if(mp.find(ch)!=mp.end()){
-                mp[ch]++;
-                right[i]=right[i+1];                   
-            }
-            else{
-                right[i]=right[i+1]+1;
-                mp[ch]++;
-            }
-        }
         int ans=0;
-        for(int i=1;i<n;i++){
-            if(left[i]==right[i-1]){
+        vector<int>left(n,0);
+       // vector<int>right(n,0);
+        unordered_map<char,int>mp;
+        set<int>l_used;
+        int l_score=0,r_score=0;
+        for(auto it:s){
+            mp[it]++;
+        }
+        r_score=mp.size();
+        for(int i=0;i<=n-2;i++){
+            char ch=s[i];
+            mp[ch]--;
+            if(l_used.find(ch)==l_used.end()){
+                l_used.insert(ch);
+                l_score++;
+            }
+            if(mp[ch]==0){
+                r_score--;
+            }
+            if(l_score==r_score){
                 ans++;
             }
         }
+       
         return ans;
     }
 };
