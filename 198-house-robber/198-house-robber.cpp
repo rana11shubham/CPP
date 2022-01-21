8a4093505e817ecc1c60420ea724e5fb2e5645af
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-01-22 00:22:52 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (72.16%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int dp[1000];
     int solve(vector<int>&nums,int pos){
-        if(pos>=nums.size()){
+        if(pos<0){
             return 0;
         }
         int &ans=dp[pos];
         if(ans!=-1){
             return ans;
         }
-        ans=max(solve(nums,pos+1),solve(nums,pos+2)+nums[pos]);
+        ans=max(solve(nums,pos-1),solve(nums,pos-2)+nums[pos]);
         
         return ans;
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
         memset(dp,-1,1000);
-        return solve(nums,0);
+        return solve(nums,n-1);
           
         //return op1;
     }
