2800c336b9910542370fb26d2d5bcbc41198c3e3
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-01-27 18:39:53 +0530

    Added solution - LeetHub

--- Number of paths - GFG/number-of-paths.cpp
+++ Number of paths - GFG/number-of-paths.cpp
@@ -6,22 +6,10 @@ using namespace std;
  // } Driver Code Ends
 long long  numberOfPaths(int m, int n)
 {
-    // Code Here
-    long long dp[m][n]={0};
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(i==0){
-                dp[i][j]=1;
-            }
-            else if(j==0){
-                dp[i][j]=1;
-            }
-            else{
-                dp[i][j]=dp[i][j-1]+dp[i-1][j];
-            }
-        }
+    if(m==1 || n==1){
+        return 1;
     }
-    return dp[m-1][n-1];
+    return numberOfPaths(m-1,n)+numberOfPaths(m,n-1);
 }
 
 // { Driver Code Starts.
