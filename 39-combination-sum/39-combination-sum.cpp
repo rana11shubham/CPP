b4cf877bca69c3d09246204cd1831a7abdd832dd
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-02-09 15:59:23 +0530

    Time: 4 ms (93.42%), Space: 10.9 MB (58.78%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-   set<vector<int>>result;
+   vector<vector<int>>result;
     void solve(vector<int>&A,int B,int pos,vector<int>&ans){
         if(B==0){
             sort(ans.begin(),ans.end());
-            result.insert(ans);
+            result.push_back(ans);
             return;
         }
         while(pos<A.size() && B-A[pos]>=0){
@@ -21,9 +21,6 @@ public:
         sort(A.begin(),A.end());
         A.erase(unique(A.begin(), A.end()), A.end());
         solve(A,B,0,ans);
-        vector<vector<int>>res;
-        for(auto it:result)
-            res.push_back(it);
-        return res;
+        return result;
     }
 };
