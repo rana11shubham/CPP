9c42b973ac0c521898ea008e6c14010a1b97a206
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-05-25 17:56:32 +0530

    Time: 70 ms (19.49%), Space: 18.2 MB (14.77%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -4,41 +4,40 @@ public:
        int n=grid.size();
         int m=grid[0].size();
         int count=0;
-        vector<vector<bool>>vis(n,vector<bool>(m,0));
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
+                if(grid[i][j]=='1'){
                     count++;
                     queue<pair<int,int>>q;
                     q.push({i,j});
-                    vis[i][j]=1;
+                    grid[i][j]='0';
                     while(!q.empty()){
                         auto it=q.front();
                         q.pop();
                         int r=it.first;
                         int c=it.second;
-                        if(r-1>=0 && !vis[r-1][c] && grid[r-1][c]=='1')
+                        if(r-1>=0 && grid[r-1][c]=='1')
                         {
-                            vis[r-1][c]=1;
+                            grid[r-1][c]='0';
                             q.push({r-1,c});
                         }
                         
-                        if(c-1>=0 && !vis[r][c-1] && grid[r][c-1]=='1')
+                        if(c-1>=0 && grid[r][c-1]=='1')
                         {
-                            vis[r][c-1]=1;
+                            grid[r][c-1]='0';
                             q.push({r,c-1});
                         }
                         
-                        if(r+1<=n-1 && !vis[r+1][c] && grid[r+1][c]=='1')
+                        if(r+1<=n-1 && grid[r+1][c]=='1')
                         {
-                            vis[r+1][c]=1;
+                            grid[r+1][c]='0';
                             q.push({r+1,c});
                         }
                         
-                        if(c+1<=m-1 && !vis[r][c+1] && grid[r][c+1]=='1')
+                        if(c+1<=m-1 && grid[r][c+1]=='1')
                         {
-                            vis[r][c+1]=1;
+                            grid[r][c+1]='0';
                             q.push({r,c+1});
                         }
                     }
