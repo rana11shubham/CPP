28f69101683dfaeaffa9970d2e90800aec8088fa
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-05-26 03:56:40 +0530

    Time: 12 ms (58.04%), Space: 14.6 MB (5.75%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,49 +1,37 @@
 class Solution {
 public:
-        int visited[101][101];
-    int dp[101][101];
-    void solve(vector<vector<int>>&image,int x,int y,int C,int r,int c){
-        if(x<0 | x>r)return;
-        if(y<0 | y>c)return;
-        if((x+1)<r){
-            if(image[x][y]==image[x+1][y] && visited[x+1][y]!=true){
-                dp[x+1][y]=C,visited[x+1][y]=true;
-                solve(image,x+1,y,C,r,c);
-            }
-        }
-        if((x-1)>=0){
-            if(image[x][y]==image[x-1][y] && visited[x-1][y]!=true){
-                dp[x-1][y]=C,visited[x-1][y]=true;
-                solve(image,x-1,y,C,r,c);
-            }
-        }
-            if((y+1)<c){
-                if(image[x][y]==image[x][y+1] && visited[x][y+1]!=true){
-                dp[x][y+1]=C,visited[x][y+1]=true;
-                solve(image,x,y+1,C,r,c);
-            }
-        }
-            if((y-1)>=0){
-                if(image[x][y]==image[x][y-1] && visited[x][y-1]!=true){
-                dp[x][y-1]=C,visited[x][y-1]=true;
-                solve(image,x,y-1,C,r,c);
-            }
-        }
-    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-         int row=image.size();
-        int col=image[0].size();
-        memset(visited,false,sizeof(visited));
-        for(int i=0;i<row;i++)
-            for(int j=0;j<col;j++)
-                dp[i][j]=image[i][j];
-        dp[sr][sc]=newColor;
-        visited[sr][sc]=true;
-        solve(image,sr,sc,newColor,row,col);
-        vector<vector<int>>ans(row,vector<int>(col));
-        for(int i=0;i<row;i++)
-            for(int j=0;j<col;j++)
-                ans[i][j]=dp[i][j];
-        return ans;
+         int n=image.size();
+        int m=image[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        vis[sr][sc]=1;
+        while(q.empty()!=true){
+            auto it=q.front();
+            q.pop();
+            int r=it.first;
+            int c=it.second;
+            int col=image[r][c];
+            // Upward direction
+            if(r-1>=0 && !vis[r-1][c] && col==image[r-1][c]){
+                vis[r-1][c]=1;
+                q.push({r-1,c});
+            }
+             if(r+1<=n-1 && !vis[r+1][c] && col==image[r+1][c]){
+                vis[r+1][c]=1;
+                q.push({r+1,c});
+            }
+             if(c-1>=0 && !vis[r][c-1] && col==image[r][c-1]){
+                vis[r][c-1]=1;
+                q.push({r,c-1});
+            }
+             if(c+1<=m-1 && !vis[r][c+1] && col==image[r][c+1]){
+                vis[r][c+1]=1;
+                q.push({r,c+1});
+            }
+            image[r][c]=newColor;
+        }
+        return image;
     }
 };
