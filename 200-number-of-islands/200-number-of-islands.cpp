83382c5af6d11c1ef087fc619937072c886361f9
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-05-25 17:44:16 +0530

    Time: 83 ms (13.14%), Space: 18.5 MB (12.65%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+       int n=grid.size();
+        int m=grid[0].size();
+        int count=0;
+        vector<vector<bool>>vis(n,vector<bool>(m,0));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    count++;
+                    queue<pair<int,int>>q;
+                    q.push({i,j});
+                    vis[i][j]=1;
+                    while(!q.empty()){
+                        auto it=q.front();
+                        q.pop();
+                        int r=it.first;
+                        int c=it.second;
+                        if(r-1>=0 && !vis[r-1][c] && grid[r-1][c]=='1')
+                        {
+                            vis[r-1][c]=1;
+                            q.push({r-1,c});
+                        }
+                        
+                        if(c-1>=0 && !vis[r][c-1] && grid[r][c-1]=='1')
+                        {
+                            vis[r][c-1]=1;
+                            q.push({r,c-1});
+                        }
+                        
+                        if(r+1<=n-1 && !vis[r+1][c] && grid[r+1][c]=='1')
+                        {
+                            vis[r+1][c]=1;
+                            q.push({r+1,c});
+                        }
+                        
+                        if(c+1<=m-1 && !vis[r][c+1] && grid[r][c+1]=='1')
+                        {
+                            vis[r][c+1]=1;
+                            q.push({r,c+1});
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
