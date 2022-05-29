d40da2d9c819502cdd17ffbb33598981ed7ab8a5
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-05-29 21:50:16 +0530

    Time: 374 ms (20.05%), Space: 21.6 MB (23.52%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
+    int uncommon(vector<vector<int>>&mp,int i,int j){
+        for(int k=0;k<26;k++){
+            if(mp[i][k]==mp[j][k] && mp[i][k]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
     int maxProduct(vector<string>& words) {
         int ans=0;
         int n=words.size();
-        unordered_map<char,int>mp;
+        vector<vector<int>>mp(n,vector<int>(26,0));
         for(int i=0;i<n;i++){
-            for(auto it:words[i])
-                mp[it]++;
-            for(int j=i+1;j<n;j++){
-                int flag=0;
-                for(auto x:words[j]){
-                    if(mp[x]){
-                        flag=1;
-                        break;
-                    }
-                }
-                    if(flag)
-                        continue;
-                    else{
+            for(auto it:words[i]){
+                mp[i][it-'a']=1;
+            }
+            for(int j=0;j<i;j++){
+                if(!uncommon(mp,i,j))
+                    continue;
+                else{
                     int len=words[i].length()*words[j].length();
-                   // cout<<words[i]<<" "<<words[j]<<endl;
+                    //cout<<words[i]<<" "<<words[j]<<endl;
                     ans=max(ans,len);
                 }
             }
-            mp.clear();
         }
         return ans;
     }
