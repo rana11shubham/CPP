f7a99cc4b9da7c0c183cee9f2302a472e3ac966c
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-06-02 15:03:31 +0530

    Time: 11 ms (34.83%), Space: 9.1 MB (26.21%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int solve(int m,int n,int col,vector<vector<int>>&triangle){
+    int solve(int m,int n,int row,int col,vector<vector<int>>&triangle){
         // Base cases
-          if(m<0 or n<0 or n>col)
+          if(m>row or n>col)
             return 1e9+7;
-        if(m==0){
+        if(m==row){
             return triangle[m][n];
         }
       
         if(dp[m][n]!=-1)
             return dp[m][n];
         
-        int downSum=triangle[m][n]+solve(m-1,n,m-1,triangle);
-        int nextdownSum=triangle[m][n]+solve(m-1,n-1,m-1,triangle);
+        int downSum=triangle[m][n]+solve(m+1,n,row,col,triangle);
+        int nextdownSum=triangle[m][n]+solve(m+1,n+1,row,col,triangle);
         return dp[m][n]=min(downSum,nextdownSum);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
@@ -21,10 +21,8 @@ public:
     int col=triangle[row-1].size();
     dp.resize(row,vector<int>(col,-1));
     int ans=INT_MAX;
-    for(int i=0;i<col;i++)
-    {
-        ans=min(ans,solve(row-1,i,row-1,triangle));
-    }
+        return solve(0,0,row-1,col-1,triangle);
+    
         return ans;
     }
 };
