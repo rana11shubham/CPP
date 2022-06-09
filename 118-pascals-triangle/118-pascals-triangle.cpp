1baa0ec57b48f5cacab80b7de85f2296f86dadfd
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-06-09 22:55:17 +0530

    Time: 3 ms (35.88%), Space: 7.6 MB (33.01%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    vector<int>solve(int n){
-    if(n==1){
-        vector<int>temp;
-        temp.push_back(1);
-        return temp;
+    vector<vector<int>>final_ans;
+    vector<int> solve(int n){
+        if(n==1)
+            return {1};
+        if(n==2)
+            return {1,1};
+        vector<int>prev_Pascal=solve(n-1);
+        vector<int>ans;
+        ans.push_back(1);
+        for(int i=0;i<prev_Pascal.size()-1;i++){
+            ans.push_back((prev_Pascal[i]+prev_Pascal[i+1]));
         }
-        else if(n==2){
-        vector<int>temp;
-        temp.push_back(1);
-        temp.push_back(1);
-        }    
-        vector<int>temp=solve(n-1);
-        vector<int>ans1;
-        ans1.push_back(1);
-        for(int i=0;i<temp.size()-1;i++){
-            ans1.push_back((temp[i]+temp[i+1]));
-        }
-        ans1.push_back(1);
-        ans.push_back(ans1);
-        return ans1;
+        ans.push_back(1);
+        return ans;
     }
     vector<vector<int>> generate(int numRows) {
-        ans.push_back({1});
-        auto it=solve(numRows);
-        return ans;
+        for(int i=1;i<=numRows;i++)
+        final_ans.push_back(solve(i));
+        return final_ans;
     }
 };
