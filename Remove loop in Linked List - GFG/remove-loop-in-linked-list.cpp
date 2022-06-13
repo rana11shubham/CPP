83eb47adf76eb039e5b05440d1b90893816552f3
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-06-13 22:17:40 +0530

    Added solution - LeetHub

---
+++ Remove loop in Linked List - GFG/remove-loop-in-linked-list.cpp
@@ -0,0 +1,151 @@
+// { Driver Code Starts
+// driver code
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head)
+{
+    if(!head) return false;
+    
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while( fast != slow)
+    {
+        if( !fast || !fast->next ) return false;
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    
+    return true;
+}
+
+int length(Node* head)
+{
+    int ret = 0;
+    while(head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+
+ // } Driver Code Ends
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    //Function to remove a loop in the linked list.
+    Node* detectCycle(Node*head){
+        Node*fast=head;
+        Node*slow=head;
+        while(fast!=NULL and fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)
+                return slow;
+        }
+        return NULL;
+    }
+    void removeLoop(Node* head)
+    {
+        Node* start=detectCycle(head);
+        if(start==NULL)
+            return;
+        // Let me find out the starting node for the cycle
+        Node*temp=head;
+        if(start==head){
+            while(temp->next!=start){
+                temp=temp->next;
+            }
+            temp->next=NULL;
+            return;
+        }
+        Node*prev=NULL;
+        while(temp!=start){
+            prev=start;
+            start=start->next;
+            temp=temp->next;
+        }
+        prev->next=NULL;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        Solution ob;
+        ob.removeLoop(head);
+        
+        if( isLoop(head) || length(head)!=n )
+            cout<<"0\n";
+        else
+            cout<<"1\n";
+    }
+	return 0;
+}
+  // } Driver Code Ends
