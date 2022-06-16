e2e65787eaf4f0b160a382df6e35596c3c82536d
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-06-16 22:13:29 +0530

    Time: 44 ms (69.90%), Space: 21.3 MB (47.08%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -6,31 +6,33 @@ public:
         int n=s.length();
         int max_len=0;
         int start=0;
-        vector<vector<bool>>dp(n+1,vector<bool>(n+1,false));
-        for(int i=0;i<n;i++){
-            dp[i][i]=true;
-            start=i;
-            max_len=1;
-        }
-        for(int i=0;i<n-1;i++){
-            if(s[i]==s[i+1]){
-                dp[i][i+1]=true;
-                start=i;
-                max_len=2;
-            }
-        }
-        for(int chain_len=3;chain_len<=n;chain_len++){
-            for(int i=0;i<n-chain_len+1;i++){
-                int j=i+chain_len-1;
-                if(s[i]==s[j]){
-                    dp[i][j]=dp[i+1][j-1];
-                    if(dp[i][j]){
-                        start=i;
-                        max_len=j-i+1;
+       string best_string="";
+        for(int mid=0;mid<n;mid++){
+            for(int i=0;mid-i>=0 and mid+i<n;i++){
+                if(s[mid-i]!=s[mid+i])
+                    break;
+                else{
+                    int len=2*i+1;
+                    if(max_len<len){
+                        max_len=len;
+                        best_string=s.substr(mid-i,max_len);
                     }
                 }
             }
         }
-        return s.substr(start,max_len);
+        for(int mid=0;mid<n-1;mid++){
+            for(int i=1;mid-i+1>=0 and mid+i<n;i++){
+                if(s[mid-i+1]!=s[mid+i])
+                    break;
+                else{
+                    int len=2*i;
+                    if(max_len<len){
+                        max_len=len;
+                        best_string=s.substr(mid-i+1,max_len);
+                    }
+                }
+            }
+        }
+        return best_string;
     }
 };
