3cf1f8679f7b6cb20f2f8a71a85de9f23ca03185
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-06-16 23:07:42 +0530

    Added solution - LeetHub

--- Solve the Sudoku - GFG/solve-the-sudoku.cpp
+++ Solve the Sudoku - GFG/solve-the-sudoku.cpp
@@ -10,72 +10,57 @@ using namespace std;
 
 
  // } Driver Code Ends
-
 class Solution 
 {
     public:
     //Function to find a solved Sudoku.
-    bool isValid(int grid[][9],int row,int col,int x){
-         for(int i=0;i<9;i++){
-            if(grid[i][col]==x ||grid[row][i]==x)
+    bool valid(int grid[][9],int row,int col,int x){
+        for(int i=0;i<9;i++){
+            if(grid[i][col]==x || grid[row][i]==x)
                 return false;
         }
         int rdx=(row/3)*3;
         int cdx=(col/3)*3;
         for(int i=rdx;i<rdx+3;i++){
-            for(int j=cdx;j<cdx+3;j++){
+            for(int j=cdx;j<cdx+3;j++)
                 if(grid[i][j]==x)
-                        return false;
-            }
+                    return false;
         }
         return true;
     }
-    bool solve(int grid[][9],int row, int col){
-        if(row==9){
+    bool solve(int grid[][9],int row,int col){
+        // Base cases
+        if(row==9)
             return true;
-        }
-        if(col==9){
-          return solve(grid,row+1,0);
-        }
-        if(grid[row][col]!=0){
-           return solve(grid,row,col+1);
-        }
+        if(col==9)
+            return solve(grid,row+1,0);
+        
+        if(grid[row][col]!=0)
+            return solve(grid,row,col+1);
+            
         for(int i=1;i<=9;i++){
-            if(isValid(grid,row,col,i)){
+            if(valid(grid,row,col,i)){
                 grid[row][col]=i;
-                bool temp=solve(grid,row,col+1);
-                if(temp==true)
-                return true;
-                
+                if(solve(grid,row,col+1)){
+                    return true;
+                }
             }
-            
-     }
-               grid[row][col]=0;
-                return false;
-      
+        }
+        grid[row][col]=0;
+        return false;
     }
-    bool SolveSudoku(int grid[N][N]){
-    // {  vector<vector<int>>dp(9,vector<int>(9));
-    //     for(int i=0;i<9;i++)
-    //         for(int j=0;j<9;j++)
-    //             dp[i][j]=grid[i][j];
-            
-        bool temp= solve(grid,0,0);
-        // for(int i=0;i<9;i++)
-        //     cout<<dp[0][i];
-        // for(int i=0;i<9;i++)
-        //     for(int j=0;j<9;j++)
-        //         grid[i][j]=dp[i][j];
-     return temp;
+    
+    bool SolveSudoku(int grid[N][N])  
+    { 
+        return solve(grid,0,0);
     }
     
     //Function to print grids of the Sudoku.
     void printGrid (int grid[N][N]) 
     {
         for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
+            for(int j=0;j<N;j++)
                 cout<<grid[i][j]<<" ";
-            }
         }
     }
 };
