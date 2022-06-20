c8adb96458cba80fca7beb6ea6522f38ad59183a
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-06-20 18:13:53 +0530

    Added solution - LeetHub

--- The Painter's Partition Problem-II - GFG/the-painters-partition-problem-ii.cpp
+++ The Painter's Partition Problem-II - GFG/the-painters-partition-problem-ii.cpp
@@ -11,38 +11,41 @@ using namespace std;
 class Solution
 {
   public:
-    bool isVerify(int arr[],long long X,int n,int k){
-        int painter=1,sum=0;
+    #define ll long long
+    bool valid(int arr[],int n,int k,ll mid){
+        long long sum=0;
+        int count=1;
         for(int i=0;i<n;i++){
             sum+=arr[i];
-            if(sum>X){
-                painter++;
+            if(sum>mid){
+                count++;
                 sum=arr[i];
             }
-            if(painter>k){
-                return false;
-            }
+            if(count>k)
+            return false;
         }
-        return true;
+      return true;
     }
     long long minTime(int arr[], int n, int k)
     {
-        // code here
-        // return minimum time
-        long long S=0,E=0;
+        ll start=0,end=0;
+       // sort(arr,arr+n);
         for(int i=0;i<n;i++){
-            S=max(S,(long long)arr[i]);
-            E+=arr[i];
+            start=max((ll)arr[i],start);
+            end+=arr[i];
         }
-        long long ans=0;
-        while(S<=E){
-            long long mid=S+(E-S)/2;
-            if(isVerify(arr,mid,n,k)){
-                E=mid-1;
+        long long ans=-1;
+        while(start<=end){
+            ll mid=start+(end-start)/2;
+            if(valid(arr,n,k,mid)){
+                // something to do here
+                end=mid-1;
                 ans=mid;
             }
-            else
-                S=mid+1;
+            else {
+                // something else to be done here
+                start=mid+1;
+            }
         }
         return ans;
     }
