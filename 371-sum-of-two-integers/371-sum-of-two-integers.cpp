17053c4b12795d609e1dab5ced00e4bfc9b8308d
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-06-27 16:59:57 +0530

    Time: 2 ms (41.53%), Space: 5.9 MB (26.53%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return (a|b) + (a&b);
-    }
+        return b==0? a:getSum(a^b, (unsigned int)(a&b)<<1);
+            
+        }
+    
 };
