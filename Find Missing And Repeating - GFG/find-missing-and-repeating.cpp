c53488402d25ec0bd44c52d0ace1962c5826958f
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-07-04 01:54:26 +0530

    Added solution - LeetHub

--- Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
+++ Find Missing And Repeating - GFG/find-missing-and-repeating.cpp
@@ -8,20 +8,38 @@ class Solution{
 public:
     int *findTwoElement(int *arr, int n) {
         int *ans=new int[2];
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
-            mp[arr[i]]++;
-        }
-        int missing=-1;
-        int twice=-1;
+        int xor_arr=0;
+        for(int i=0;i<n;i++)
+            xor_arr^=arr[i];
+        int xor_full=0;
+        for(int i=1;i<=n;i++)
+            xor_full^=i;
+        int temp=xor_full^xor_arr;
+        int set_bit=temp&(~(temp-1));
+        int a=0,b=0;
         for(int i=1;i<=n;i++){
-            if(mp[i]==0)
-                missing=i;
-            if(mp[i]==2)
-                twice=i;
+            if((i&set_bit)!=0)
+                a^=i;
+            else b^=i;
         }
-       ans[0]=twice;
-       ans[1]=missing;
+        for(int i=0;i<n;i++){
+            if((arr[i]&set_bit)!=0)
+                a^=arr[i];
+            else b^=arr[i];
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==a)
+                count=1;
+        }
+        if(count==1){
+            ans[0]=a;
+            ans[1]=b;
+        }
+        else{
+            ans[0]=b;
+            ans[1]=a;
+        }
        return ans;
     }
 };
