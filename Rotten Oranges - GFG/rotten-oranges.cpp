f1e2432fde2ad9cc5b2aa9205065d430fc107873
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-07-05 00:00:42 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -8,64 +8,46 @@ class Solution
     public:
     //Function to find minimum time required to rot all oranges. 
     int orangesRotting(vector<vector<int>>& grid) {
-     int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-         queue<pair<int,int>>q;
-        int time=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                   
-                    q.push({i,j});
-                    vis[i][j]=1;
-                }
+    int n=grid.size();
+    int m=grid[0].size();
+    queue<pair<int,int>>q;
+    for(int i=0;i<n;i++)
+        for(int j=0;j<m;j++){
+            if(grid[i][j]==2)
+            q.push({i,j});
+        }
+    int time=0;
+    int row[]={-1,0,0,1};
+    int col[]={0,-1,1,0};
+    while(q.empty()!=true){
+        int sz=q.size();
+        int status=0;
+        for(int i=0;i<sz;i++){
+            auto it=q.front();
+            q.pop();
+            int ro=it.first;
+            int co=it.second;
+            for(int j=0;j<4;j++){
+                if(ro+row[j]>=0 and ro+row[j]<n and co+col[j]>=0 and co+col[j]<m and grid[ro+row[j]][co+col[j]]==1){
+                    grid[ro+row[j]][co+col[j]]=2;
+                    q.push({ro+row[j],co+col[j]});
+                    
+                    status=1;
+                
             }
         }
-                    while(!q.empty()){
-                        int qs=q.size();
-                         int status=0;
-                        for(int x=0;x<qs;x++){
-                        auto it=q.front();
-                        q.pop();
-                        int r=it.first;
-                        int c=it.second;
-                        if(r-1>=0 && grid[r-1][c]==1 && !vis[r-1][c]){
-                            vis[r-1][c]=true;
-                            q.push({r-1,c});
-                            grid[r-1][c]=2;
-                            status=1;
-                        }
-                         if(r+1<n && grid[r+1][c]==1 && !vis[r+1][c]){
-                            vis[r+1][c]=true;
-                            q.push({r+1,c});
-                             grid[r+1][c]=2;
-                             status=1;
-                        }
-                         if(c-1>=0 && grid[r][c-1]==1 && !vis[r][c-1]){
-                            vis[r][c-1]=true;
-                            q.push({r,c-1});
-                             grid[r][c-1]=2;
-                            status=1;
-                         }
-                         if(c+1<m && grid[r][c+1]==1 && !vis[r][c+1]){
-                            vis[r][c+1]=true;
-                            q.push({r,c+1});
-                             grid[r][c+1]=2;
-                             status=1;
-                        }
-                    }
-                    if(status)
-                    time++;
-                }
-                        
-       
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
-                if(grid[i][j]==1)
-                    return -1;
-        return time;
+        }
+        if(status)
+            time++;
     }
+    for(auto it:grid)
+        for(auto it1:it)
+            if(it1==1)
+                return -1;
+                
+    return time;
+    }
+    
 };
 
 // { Driver Code Starts.
