e5796a83d8cb4f014d07486a30bf3cb296c0bccf
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-07-04 22:37:22 +0530

    Added solution - LeetHub

--- Covid Spread - GFG/covid-spread.cpp
+++ Covid Spread - GFG/covid-spread.cpp
@@ -13,60 +13,40 @@ class Solution {
 public:
     int helpaterp(vector<vector<int>> grid)
     {
-         int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
+         int r=grid.size();
+         int c=grid[0].size();
          queue<pair<int,int>>q;
-        int time=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<r;i++)
+            for(int j=0;j<c;j++){
                 if(grid[i][j]==2){
-                   
                     q.push({i,j});
-                    vis[i][j]=1;
                 }
             }
-        }
-                    while(!q.empty()){
-                        int qs=q.size();
-                         int status=0;
-                        for(int x=0;x<qs;x++){
-                        auto it=q.front();
-                        q.pop();
-                        int r=it.first;
-                        int c=it.second;
-                        if(r-1>=0 && grid[r-1][c]==1 && !vis[r-1][c]){
-                            vis[r-1][c]=true;
-                            q.push({r-1,c});
-                            grid[r-1][c]=2;
-                            status=1;
+         int time=0;
+         int row[]={-1,0,0,1};
+         int col[]={0,-1,1,0};
+         while(q.empty()!=true){
+             int n=q.size();
+              int status=1;
+             for(int i=0;i<n;i++){
+                 auto it=q.front();
+                 q.pop();
+                 int ro=it.first;
+                 int co=it.second;
+                 for(int j=0;j<4;j++){
+                     if(ro+row[j]>=0 and ro+row[j]<r and co+col[j]>=0 and co+col[j]<c and grid[ro+row[j]][co+col[j]]==1)
+                        {
+                            grid[ro+row[j]][co+col[j]]=2;
+                            q.push({ro+row[j],co+col[j]});
+                            status=0;
                         }
-                         if(r+1<n && grid[r+1][c]==1 && !vis[r+1][c]){
-                            vis[r+1][c]=true;
-                            q.push({r+1,c});
-                             grid[r+1][c]=2;
-                             status=1;
-                        }
-                         if(c-1>=0 && grid[r][c-1]==1 && !vis[r][c-1]){
-                            vis[r][c-1]=true;
-                            q.push({r,c-1});
-                             grid[r][c-1]=2;
-                            status=1;
-                         }
-                         if(c+1<m && grid[r][c+1]==1 && !vis[r][c+1]){
-                            vis[r][c+1]=true;
-                            q.push({r,c+1});
-                             grid[r][c+1]=2;
-                             status=1;
-                        }
-                    }
-                    if(status)
-                    time++;
-                }
-                        
-       
-        for(int i=0;i<n;i++)
-            for(int j=0;j<m;j++)
+                 }
+             }
+             if(status==0)
+             time++;
+         }
+         for(int i=0;i<r;i++)
+            for(int j=0;j<c;j++)
                 if(grid[i][j]==1)
                     return -1;
         return time;
