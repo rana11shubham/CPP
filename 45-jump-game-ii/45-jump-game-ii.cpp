a9f09a26f913e90bcb50ffe5409c0ec44c559f05
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-07-06 22:36:29 +0530

    Time: 541 ms (24.68%), Space: 18.2 MB (6.09%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    #define INF 1000000007
-    int jump(vector<int>& A) {
-       int n=A.size();
-        if(n==1)
+      vector<int>dp;
+    int solve(int i,vector<int>&nums){
+        // Base case
+        if(i>=nums.size()-1){
             return 0;
-       int curr_pos=A[0],next_pos=A[0];
-        int jump=1;
-        for(int i=1;i<n;i++){
-            if(curr_pos<i){
-                jump++;
-                curr_pos=next_pos;
-            }
-            next_pos=max(next_pos,A[i]+i);
         }
-        return jump;
+        int &ans=dp[i];
+        if(ans!=-1)
+            return ans;
+        int n=nums.size();
+        int temp=1e9;
+        for(int k=i+1;k<=min(n,nums[i]+i);k++){
+            temp=min(temp,1+solve(k,nums));
+        }
+        return ans= temp;
+    }
+    int jump(vector<int>& nums) {
+        dp.resize(nums.size(),-1);
+        return solve(0,nums);   
     }
 };
