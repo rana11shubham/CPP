e45cc7ade5b636493864cbd936b8130baf3cc763
Shubham Rana <36005170+rana11shubham@users.noreply.github.com>
2022-07-06 12:43:50 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -9,58 +9,59 @@ class Solution{
     // arr[]: Input Array
     // N : Size of the Array arr[]
     // Function to count inversions in the array.
-    
-    long long solve(long long arr[],long long start,long long mid,long long end){
+    long long merge(long long arr[],long long s,long long mid,long long e)
+    {
         long long count=0;
-        long long temp[end-start+1];
-        long long i=start,j=mid, k=start-start;
-        while(i<mid and j<=end){
+        long long temp[e-s+1];
+        long long i=s,j=mid;
+        long long k=0;
+        while(i<mid and j<=e){
             if(arr[i]>arr[j]){
+                count+=(mid-i);
                 temp[k]=arr[j];
                 j++;
                 k++;
-                count+=(mid-i);
+                
             }
-            else{
+            else {
                 temp[k]=arr[i];
                 i++;
                 k++;
             }
         }
-        while(i<mid){
+        while(i<mid)
+        {
             temp[k]=arr[i];
+            k++;
             i++;
-            k++;
-        }
-        while(j<=end){
+        }   
+        while(j<=e)
+        {
             temp[k]=arr[j];
-            j++;
             k++;
-            
-        }
-        for(int x=start;x<=end;x++){
-            arr[x]=temp[x-start];
-            //cout<<arr[x]<<" ";
-        }
-       // cout<<endl;
-      return count;
-    }
-    long long int mergeSort(long long arr[],long long start,long long end){
-        long long count=0;
-        if(start<end){
-            long long mid=(start+end)/2;
-            count+=mergeSort(arr,start,mid);
-            count+=mergeSort(arr,mid+1,end);
-            count+=solve(arr,start,mid+1,end);
+            j++;
+        }   
+        
+        for(int m=s;m<=e;m++){
+            arr[m]=temp[m-s];
         }
         return count;
     }
-   
+    
+    long long mergeSort(long long arr[],long long s,long long e){
+        long long ans=0;
+        if(s<e){
+            long long mid=s+(e-s)/2;
+            ans+=mergeSort(arr,s,mid);
+            ans+=mergeSort(arr,mid+1,e);
+            ans+=merge(arr,s,mid+1,e);
+        }
+        return ans;
+    }
     long long int inversionCount(long long arr[], long long N)
     {
         return mergeSort(arr,0,N-1);
-       // return count;
-    }
+        }
 
 };
 
